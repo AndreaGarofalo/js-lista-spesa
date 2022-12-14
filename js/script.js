a0afd7b8c4de984ebf3fd31d8d258cfe2feb695c
AndreaGarofalo <andreasjobs94@outlook.it>
2022-12-14 17:53:26 +0100

    finished exercise

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log("JS OK");
+
+const shoppingList = ["mele", "acqua", "pasta", "dentifricio"];
+
+const listElement = document.getElementById("list");
+
+let i = 0;
+
+while (i < shoppingList.length) {
+  const item = document.createElement("li");
+  item.innerText = shoppingList[i];
+  listElement.appendChild(item);
+  i++;
+}
